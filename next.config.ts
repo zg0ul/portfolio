6b5a5135e27cc2f2872b63d9fce4f63e459c5eb6
Mohammad Zgoul <moeazgoul@gmail.com>
2025-05-30 01:15:23 +0300

    fixed

--- next.config.ts
+++ next.config.ts
@@ -65,15 +65,12 @@ const nextConfig: NextConfig = {
   devIndicators: false,
   images: {
     dangerouslyAllowSVG: true,
-    contentDispositionType: "attachment",
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
     remotePatterns: [
       {
         protocol: "https",
         hostname: "**", // Wildcard to allow all hostnames
       },
     ],
-    domains: ["via.placeholder.com", "img.shields.io"],
   },
 };
 
